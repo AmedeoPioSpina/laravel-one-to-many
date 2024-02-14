2451fc3aa2377de9494498e742a0c9c5c646323c
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2024-02-14 18:40:57 +0100

    db:seed, problem whit data

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        //
+        for($i=0; $i > 10; $i++){
+            $newProject = new Project();
+            $newProject->type_id = rand(0, 10);
+            $newProject->name = $faker->word();
+            $newProject->save();
+        }
+    }
+}
---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        //
+        for($i=0; $i > 10; $i++){
+            $newType = new Type();
+            $newType->name = $faker->words(1, true);
+            $newType->save();
+        }
+    }
+}
